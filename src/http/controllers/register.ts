743a02830d544b3364334cbe6735e639fbaaebe6
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2026-03-14 22:48:29 -0300

    Feat: Criação de tabelas e rota de login

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma.js'
+import { hash } from 'bcryptjs'
+import type { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  })
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'Email already exists.',
+    })
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+
+  return reply.status(201).send()
+}
